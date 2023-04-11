cc747b86479f19f5f2c49f79859d1a4e0d14d3b5
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2023-04-11 12:18:33 +0200

    added cors module

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3001;
 
 let items = []; // This will act as our in-memory database
 let itemId = 0;
 
+app.use(cors());
 app.use(express.json());
 
 // GET items
